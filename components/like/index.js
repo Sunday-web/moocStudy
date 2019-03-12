ee012aa872aac94a67754654134cfb9094044533
Sunday <775011185@qq.com>
2019-03-12 17:56:46 +0800

    api请求

---
+++ components/like/index.js
@@ -0,0 +1,44 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like:{
+      type:Boolean
+      //value:false
+      //默认值可以省略 默认false
+    },
+    count:{
+      type:Number
+      //value:0
+      //默认值可以省略 默认0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // like:true,
+    // count:9,
+    yesSrc:'images/like.png',
+    noSrc:'images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike:function(e){
+      let like=this.properties.like
+      let count = this.properties.count
+      count=like?count-1:count+1
+      this.setData({
+        like: !like,
+        count: count
+        
+      })
+    }
+  }
+})
