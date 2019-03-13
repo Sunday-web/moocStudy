261fb298b51ea5c6b19a9fad6be228e351c9615d
Sunday <775011185@qq.com>
2019-03-13 17:52:02 +0800

    点赞请求

--- components/like/index.js
+++ components/like/index.js
@@ -31,14 +31,17 @@ Component({
    */
   methods: {
     onLike:function(e){
+      //自定义事件
       let like=this.properties.like
       let count = this.properties.count
       count=like?count-1:count+1
       this.setData({
         like: !like,
         count: count
-        
       })
+      //激活
+      let behavior=this.properties.like?'like':'cancel'
+      this.triggerEvent('like', { behavior: behavior},{})
     }
   }
 })
---
+++ config.js
@@ -0,0 +1,5 @@
+const config={
+  api_base_url:'http://bl.7yue.pro/v1/',
+  appkey:"GlMAIptMoSp0xOnT"
+}
+export {config}
